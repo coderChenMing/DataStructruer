ce8afdf49f20b393827115f3a4fd450e3c5b40e8
chenming1 <280433706@qq.com>
2023-02-28 11:45:26 +0800

    前中后序遍历层序遍历

--- 二叉树/src/com/learn/BinarySearchTree.java
+++ 二叉树/src/com/learn/BinarySearchTree.java
@@ -2,15 +2,34 @@ package com.learn;
 
 import com.learn.printer.BinaryTreeInfo;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 二叉搜索树:
  * 任意节点的左子树所有节点值小于当前节点值，右子树所有节点值大于当前节点值
- * 节点必须是可以比较的
+ * <p>
+ * 节点必须是可以比较的（直接实现Comparable接口,或者自定义比较器内容，如果是引用对象，引用对象要入二叉树的属性符合前面两种情况）
+ * <p>
  * 应用：可以思考下，mysql数据库按照某个字段进行排序查询的过程
  */
-public class BinarySearchTree<E extends Comparable<E>> implements BinaryTreeInfo {
+public class BinarySearchTree<E> implements BinaryTreeInfo {
+    //public class BinarySearchTree<E extends Comparable<E>> implements BinaryTreeInfo {
+    // 使用BinarySearchTree<E extends Comparable<E>> 方式，第三方调用必须保证添加的元素实现Comparable接口，不太友好
+    // 可以增加比较器，由第三方确定比较规则，这样可以比较灵活调用
     private int size;
     private Node<E> root;
+
+    private Comparator<E> comparator;
+
+    public BinarySearchTree() {
+
+    }
+
+    public BinarySearchTree(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
 
     public int size() {
         return size;
@@ -18,6 +37,11 @@ public class BinarySearchTree<E extends Comparable<E>> implements BinaryTreeInfo
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    public void clear() {
+        root = null;
+        size = 0;
     }
 
     // 添加元素
@@ -42,6 +66,7 @@ public class BinarySearchTree<E extends Comparable<E>> implements BinaryTreeInfo
             } else if (result < 0) {
                 node = node.left;
             } else {
+                node.element = element;// 是否有意义？有的，比如同龄不同名，直接替换新名
                 return;
             }
         }
@@ -60,7 +85,10 @@ public class BinarySearchTree<E extends Comparable<E>> implements BinaryTreeInfo
      * result<0 : e1<e2
      */
     private int compare(E e1, E e2) {
-        return e1.compareTo(e2);
+        if (null != comparator) {
+            return comparator.compare(e1, e2);
+        }
+        return ((Comparable) e1).compareTo(e2);
     }
 
     private void checkElement(E element) {
@@ -69,6 +97,111 @@ public class BinarySearchTree<E extends Comparable<E>> implements BinaryTreeInfo
         }
     }
 
+    // 四种遍历方式: 前中后序遍历,层序遍历
+    // 不局限于二叉搜索树，适用于所有二叉树
+
+    /**
+     * 前序遍历
+     * 前序遍历:先遍历根节点，再遍历左子树，再遍历右子树
+     * 对于左子树：先遍历左子树根节点，再遍历左子树的左子树，再遍历左子树的右子树。。。。
+     * 建议画图走流程
+     */
+    public void preorderTraversal() {
+        Node<E> node = root;
+        preorderTraversal(node);
+    }
+
+    private void preorderTraversal(Node<E> node) {
+        if (null == node) {
+            return;
+        }
+        System.out.println(node.element);
+        preorderTraversal(node.left);
+        preorderTraversal(node.right);
+    }
+
+    /**
+     * 中序遍历
+     * 先遍历左子树，再遍历根节点，再遍历右子树
+     * 遍历左子树，先遍历左子树的左子树，再遍历左子树的根节点，再遍历左子树的右子树 ....
+     * 对于二叉搜索树:中序遍历 各节点数据会从小到大，或者从大到小排序（由比较器比较规则控制）
+     */
+    public void inorderTraversal() {
+        Node<E> node = root;
+        inorderTraversal(node);
+    }
+
+    private void inorderTraversal(Node<E> node) {
+        if (null == node) {
+            return;
+        }
+        inorderTraversal(node.left);
+        System.out.println(node.element);
+        inorderTraversal(node.right);
+    }
+
+    /**
+     * 后序遍历
+     * 先遍历左子树，再遍历右子树，最后遍历根节点
+     * 遍历左子树：先遍历左子树的左子树，再遍历左子树的右子树，再遍历左子树的根节点
+     */
+    public void postorderTraversal() {
+        Node<E> node = root;
+        postorderTraversal(node);
+    }
+
+    private void postorderTraversal(Node<E> node) {
+        if (null == node) {
+            return;
+        }
+        postorderTraversal(node.left);
+        postorderTraversal(node.right);
+        System.out.println(node.element);
+    }
+
+    /**
+     * 层序遍历
+     * 从根节点开始从上向下，从左向右一层一层的遍历
+     * 遍历过程类似队列入队出队，使用队列实现
+     */
+    public void levelOrderTraversal() {
+        Node<E> node = root;
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+            System.out.println(node.element);
+            // 从左向右，先向队尾加左
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            // 从左向右，后向队尾加右节点
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        preorderTraversalToString(root, sb);
+        return sb.toString();
+    }
+
+    private void preorderTraversalToString(Node<E> node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+        sb.append(node.element);
+        preorderTraversalToString(node.left, sb);
+        preorderTraversalToString(node.right, sb);
+    }
+
+
+    // 工具类打印器的使用
     @Override
     public Object root() {
         return root;
@@ -87,6 +220,10 @@ public class BinarySearchTree<E extends Comparable<E>> implements BinaryTreeInfo
     @Override
     public Object string(Object node) {
         return ((Node<E>) node).element;
+        // 添加打印父节点元素
+       /* Node<E> newNode = (Node<E>) node;
+        String parent = newNode.parent == null ? "null" : newNode.parent.element+"";
+        return " parent_" + parent + " > ele_"+ newNode.element;*/
     }
 
     private static class Node<E> {
--- 二叉树/src/com/learn/BinarySearchTreeTest.java
+++ 二叉树/src/com/learn/BinarySearchTreeTest.java
@@ -3,22 +3,85 @@ package com.learn;
 import com.learn.printer.BinaryTrees;
 
 public class BinarySearchTreeTest {
+
+    private static Integer data[] = new Integer[]{
+            7, 4, 9, 2, 5, 8, 11,10, 3, 12, 1
+    };
+
+    private static Person[] people = {
+            new Person(7, "吕布"),
+            new Person(4, "赵云"),
+            new Person(9, "典韦"),
+            new Person(2, "关羽"),
+            new Person(5, "马超"),
+            new Person(8, "张飞"),
+            new Person(11, "黄忠"),
+            new Person(3, "许褚"),
+            new Person(12, "张郃"),
+            new Person(1, "张辽"),
+            new Person(12, "徐晃")
+    };
+
     public static void main(String[] args) {
+        // test1();
+        // test2();
+        // test3();
+        // test4();
+        test5();
+    }
+
+    public static void test1() {
+        // 添加功能测试
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        bst.add(7);
-        bst.add(4);
-        bst.add(9);
-        bst.add(2);
-        bst.add(5);
-        bst.add(8);
-        bst.add(11);
-        bst.add(1);
-        bst.add(3);
-        bst.add(10);
-        bst.add(12);
-
-        System.out.println(bst.size());
+        for (int i = 0; i < data.length; i++) {
+            bst.add(data[i]);
+        }
+        //System.out.println(bst.size());
         BinaryTrees.println(bst);
+    }
 
+    public static void test2() {
+        // 比较器测试:通过控制二叉搜索树元素的比较顺序，打印想要的内容
+        BinarySearchTree<Person> bst = new BinarySearchTree<>((o1, o2) -> o1.getAge() - o2.getAge());
+        BinarySearchTree<Person> bst2 = new BinarySearchTree<>((o1, o2) -> o2.getAge() - o1.getAge());
+        for (int i = 0; i < people.length; i++) {
+            bst.add(people[i]);
+            bst2.add(people[i]);
+        }
+        BinaryTrees.println(bst);
+        System.out.println("=================================================================================================================");
+        BinaryTrees.println(bst2);
+    }
+
+    public static void test3() {
+        // 测试节点元素相等
+        //  new Person(12, "张郃"),
+        //  new Person(12, "徐晃")
+        BinarySearchTree<Person> bst = new BinarySearchTree<>((o1, o2) -> o1.getAge() - o2.getAge());
+        for (int i = 0; i < people.length; i++) {
+            bst.add(people[i]);
+        }
+        BinaryTrees.println(bst);
+    }
+
+    public static void test4() {
+        // 测试前中后序遍历,层序遍历
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst.add(data[i]);
+        }
+        //bst.preorderTraversal();
+        //bst.inorderTraversal();
+        //bst.postorderTraversal();
+        bst.levelOrderTraversal();
+    }
+
+    public static void test5() {
+        // 测试toString方法
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst.add(data[i]);
+        }
+        System.out.println(bst);
     }
 }
